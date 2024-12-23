d9efd31193e880fded857bf8b76dfc557b79e66d
apurvab <apurvab@consultbop.com>
2024-12-23 15:07:23 +0530

    refactor: use memoized data

--- src/modules/builder/resume/ResumeLayout.tsx
+++ src/modules/builder/resume/ResumeLayout.tsx
@@ -1,4 +1,4 @@
-import { Context, createContext} from 'react';
+import { Context, createContext, useMemo} from 'react';
 import { useResumeStore } from '../../../stores/useResumeStore';
 import FormalTemplate from '../../../templates/formal/FormalTemplate';
 
@@ -6,12 +6,14 @@ export let StateContext: Context<any> = createContext(null);
 
 export const ResumeLayout = () => {
   const resumeData = useResumeStore();
+const memoizedData = useMemo(() => resumeData, [resumeData]); 
+
 
   return (
     <div className="mx-5 print:mx-0 mb-2 print:mb-0">
       <div>
         <div className="w-[210mm] h-[296mm] bg-white my-0 mx-auto">
-          <StateContext.Provider value={resumeData}>
+          <StateContext.Provider value={memoizedData}>
             <FormalTemplate/>
           </StateContext.Provider>
         </div>
