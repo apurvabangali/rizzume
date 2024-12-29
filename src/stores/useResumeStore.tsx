16bb554ce77ea47c7b7aaf80977b4cd546a04e12
apurvab <apurvab@consultbop.com>
2024-12-29 13:23:00 +0530

    wip: skills section

--- src/stores/useResumeStore.tsx
+++ src/stores/useResumeStore.tsx
@@ -3,7 +3,7 @@ import { useBasicDetails } from './basic';
 import { useEducations } from './education';
 import { useExperiences } from './experience';
 import { useProjects } from './projects';
-import { useLanguages, useFrameworks, useTechnologies, useLibraries, useDatabases, usePractices, useTools } from './skill';
+import { useLanguages } from './skill';
 
 
 
@@ -14,28 +14,29 @@ export const useResumeStore = () => {
     education: useEducations((state) => state.academics),
     work: useExperiences((state) => state.experiences),
     personalProjects:useProjects((state)=>state.projects),
-    skills: {
-      languages: useLanguages((state) => state.get()),
-      frameworks: useFrameworks((state) => state.get()),
-      technologies: useTechnologies((state) => state.get()),
-      libraries: useLibraries((state) => state.get()),
-      databases: useDatabases((state) => state.get()),
-      practices: usePractices((state) => state.get()),
-      tools: useTools((state) => state.get()),
-    },
+    // skills: {
+    //   languages: useLanguages((state) => state.get()),
+    //   frameworks: useFrameworks((state) => state.get()),
+    //   technologies: useTechnologies((state) => state.get()),
+    //   libraries: useLibraries((state) => state.get()),
+    //   databases: useDatabases((state) => state.get()),
+    //   practices: usePractices((state) => state.get()),
+    //   tools: useTools((state) => state.get()),
+    // },
   };
 };
 
 export const resetResumeStore = () => {
   useBasicDetails.getState().reset(ResumeData.basics);
   useLanguages.getState().reset(ResumeData.skills.languages);
-  useFrameworks.getState().reset(ResumeData.skills.frameworks);
-  useLibraries.getState().reset(ResumeData.skills.libraries);
-  useDatabases.getState().reset(ResumeData.skills.databases);
-  useTechnologies.getState().reset(ResumeData.skills.technologies);
-  usePractices.getState().reset(ResumeData.skills.practices);
-  useTools.getState().reset(ResumeData.skills.tools);
+  // useFrameworks.getState().reset(ResumeData.skills.frameworks);
+  // useLibraries.getState().reset(ResumeData.skills.libraries);
+  // useDatabases.getState().reset(ResumeData.skills.databases);
+  // useTechnologies.getState().reset(ResumeData.skills.technologies);
+  // usePractices.getState().reset(ResumeData.skills.practices);
+  // useTools.getState().reset(ResumeData.skills.tools);
   useExperiences.getState().reset(ResumeData.work);
   useEducations.getState().reset(ResumeData.education);
+  useProjects.getState().reset(ResumeData.projects)
   
 };
