5406b33e6bfcef865f5922ab498da4c605c27945
apurvab <apurvab@consultbop.com>
2025-01-12 14:29:59 +0530

    fix: move jodit editor setup to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,30 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx'
-import './index.css'
+import App from './App.tsx';
+import './index.css';
 import { Bounce, ToastContainer } from 'react-toastify';
+import 'jodit/es2021/jodit.min.css'; 
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter >
-    <App />
-    <ToastContainer
-      position="bottom-right"
-      autoClose={3000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick={false}
-      rtl={false}
-      draggable
-      theme="light"
-      transition={Bounce}
-    />
-  </BrowserRouter >,
-)
+function Main() {
+ 
+  return (
+    <>
+      <BrowserRouter>
+        <App />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          draggable
+          theme="light"
+          transition={Bounce}
+        />
+      </BrowserRouter> 
+    </>
+  );
+}
+
+createRoot(document.getElementById('root')!).render(<Main />);
